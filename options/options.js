cf4390ecf9ee17013aea81005b3d011ae9acaeb8
Crowdbotics <engineering@crowdbotics.com>
2023-01-24 14:11:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lama-two-dev-59164",
+  url: "https://lama-two-dev-59164.botics.co",
+  api: "https://lama-two-dev-59164.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
